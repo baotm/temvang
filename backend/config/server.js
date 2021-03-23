f5add1984aa44181d8e1bab6169ed4f132c4a359
unknown <trinhminhbao@gmail.com>
2021-03-23 16:26:42 +0700

    Abc

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 3002),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'fafd7566acbbb43d828ea365bd726a34'),
